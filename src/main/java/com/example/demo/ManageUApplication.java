2a5e9de49a4e54ce52201002461012ec51af6a6d
DanielTampe <d.tampe01@ufromail.cl>
2021-12-15 20:23:36 -0300

    1er Commit

---
+++ src/main/java/com/example/demo/ManageUApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageUApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageUApplication.class, args);
+	}
+
+}
